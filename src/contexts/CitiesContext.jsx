45ea5f0e99544993f32e84ecdd5fa28fe33e0c13
Prakhar <prakharkiit2017@gmail.com>
2025-02-08 23:49:48 +0530

    Updated City and CitiesContext

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,9 +1,9 @@
 import {
   createContext,
+  useCallback,
   useContext,
   useEffect,
   useReducer,
-  useState,
 } from "react";
 
 const BASE_URL = "http://localhost:9000";
@@ -92,21 +92,24 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
 
-    dispatch({ type: ACTION_TYPES.LOADING });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: ACTION_TYPES.CITY_LOADED, payload: data });
-    } catch {
-      dispatch({
-        type: ACTION_TYPES.REJECTED,
-        payload: "There was an error loading the city...",
-      });
-    }
-  }
+      dispatch({ type: ACTION_TYPES.LOADING });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: ACTION_TYPES.CITY_LOADED, payload: data });
+      } catch {
+        dispatch({
+          type: ACTION_TYPES.REJECTED,
+          payload: "There was an error loading the city...",
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: ACTION_TYPES.LOADING });
